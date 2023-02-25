c42770ea808beb3a9b2be327e713723df8482435
jacksonthompson1 <122913401+jacksonthompson1@users.noreply.github.com>
2023-02-24 22:25:55 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -18,16 +18,7 @@ $(document).ready(function () {
 
     $("#coolNameFactsOutput").html(countVowels(userName));
 
-    // $("#coolNameFactsOutput").html(getSpiritAnimal(userName));
-
-    // $("#coolNameFactsOutput").html(
-    //   '<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Y-YVLipceJt1DMU3Bs8uIAHaFj%26pid%3DApi&f=1">'
-    // );
-
-    // $("#coolNameImg").attr(
-    //   "src",
-    //   "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Y-YVLipceJt1DMU3Bs8uIAHaFj%26pid%3DApi&f=1"
-    // );
+    
   });
 });
 
@@ -39,7 +30,7 @@ function generateCoolNameFacts(userName) {
   coolFacts += "<h3> " + nameEnd(userName) + "</h3>";
   coolFacts += "<h2>" + reverseName(userName) + "</h2>";
 
-  console.log("initialized the'm coolFacts variable: " + coolFacts);
+  console.log("coolFacts var: " + coolFacts);
 
   return coolFacts;
 }
@@ -57,33 +48,12 @@ function countVowels(name) {
   return "Your name has " + count + " vowels!";
 }
 
-function nameStart(name) {
-  return "the first letter of your name is " + name[0];
-}
 
-function nameEnd(name) {
-  let lastIndex = name.length - 1;
 
-  return "the last letter of your name is " + name[lastIndex];
-}
 
-function reverseName(name) {
-  // let arrSomeArray = [1, "cat", "Susan", [j, "stuff"]];
 
-  let arrName = name.split();
+ 
 
-  revName = arrName.reverse();
-}
 
-function getSpiritAnimal(x) {
-  //flow control: case switch, loop, if/then
 
-  if (x.length == 3) {
-    //execute some code
-    return "you are slug!";
-  } else if (x.length > 10) {
-    return "u spirit animal is iguana";
-  } else {
-    return "u are nothing.";
-  }
-}
+
